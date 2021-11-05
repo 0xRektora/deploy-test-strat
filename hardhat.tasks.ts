0b093c34fe9179583faf9777a8b19152c001c39f
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-11-05 14:52:10 -0400

    Added a swap-lp task

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -3,9 +3,10 @@ import "@nomiclabs/hardhat-ethers"
 
 
 import { vaultTests } from "./tasks/vaultTests";
-import { string } from "hardhat/internal/core/params/argumentTypes";
+import { string, } from "hardhat/internal/core/params/argumentTypes";
 import { deployVault } from "./tasks/deployVault";
 import { getPools } from "./tasks/getPools";
+import { swapAndGetLp } from "./tasks/swapAndGetLp";
 
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -19,4 +20,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 task("deploy-vault", deployVault).addFlag("noCompile", "If used, will not trigger a smart contract compilation")
 task("get-pools", getPools).addParam("address", "The masterchef address", undefined, string, false)
 task("vault-test", vaultTests).addParam("address", "The deployed vault address", undefined, string, false)
+task("swap-lp", swapAndGetLp)
+    .addParam("masterchef", "The masterchef address", undefined, string, false)
+    .addParam("router", "The router address", undefined, string, false)
+    .addParam("swapAmount", "The native amount to swap", undefined, string, false)
+    .addParam("poolIds", "The pool id addresses, should be a string separated by a comma", undefined, string, false)
 
